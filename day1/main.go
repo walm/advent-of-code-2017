878e68b2938eb25f58194b04c43bf0b75631aa13
Andreas Wålm <andreas@walm.net>
2018-01-17 10:38:56 +0100

    Day 1 - part 2

--- day1/main.go
+++ day1/main.go
@@ -12,7 +12,7 @@ func btoi(b byte) int {
 	return n
 }
 
-func check(digits string) (sum int) {
+func part1(digits string) (sum int) {
 	for i, _ := range digits {
 		if i > 0 && digits[i] == digits[i-1] {
 			sum += btoi(digits[i])
@@ -24,8 +24,23 @@ func check(digits string) (sum int) {
 	return
 }
 
+func part2(digits string) (sum int) {
+	steps := len(digits) / 2
+	for i, _ := range digits {
+		nextPos := i + steps
+		if nextPos >= len(digits) {
+			nextPos = nextPos - len(digits)
+		}
+		if digits[i] == digits[nextPos] {
+			sum += btoi(digits[i])
+		}
+	}
+	return
+}
+
 func main() {
 	input, _ := ioutil.ReadFile("input.txt")
 	digits := strings.Trim(string(input), "\n")
-	fmt.Printf("Sum: %d", check(digits))
+	fmt.Printf("Part1 sum: %d\n", part1(digits))
+	fmt.Printf("Part2 sum: %d\n", part2(digits))
 }
--- day1/main_test.go
+++ day1/main_test.go
@@ -2,33 +2,73 @@ package main
 
 import "testing"
 
-func TestCheck1(t *testing.T) {
-	res := check("1122")
+func TestPart1_1(t *testing.T) {
+	res := part1("1122")
 	exp := 3
 	if res != exp {
 		t.Errorf("result missmatch, got: %d, expect: %d.", res, exp)
 	}
 }
 
-func TestCheck2(t *testing.T) {
-	res := check("1111")
+func TestPart1_2(t *testing.T) {
+	res := part1("1111")
 	exp := 4
 	if res != exp {
 		t.Errorf("result missmatch, got: %d, expect: %d.", res, exp)
 	}
 }
 
-func TestCheck3(t *testing.T) {
-	res := check("1234")
+func TestPart1_3(t *testing.T) {
+	res := part1("1234")
 	exp := 0
 	if res != exp {
 		t.Errorf("result missmatch, got: %d, expect: %d.", res, exp)
 	}
 }
 
-func TestCheck4(t *testing.T) {
-	res := check("91212129")
+func TestPart1_4(t *testing.T) {
+	res := part1("91212129")
 	exp := 9
+	if res != exp {
+		t.Errorf("result missmatch, got: %d, expect: %d.", res, exp)
+	}
+}
+
+func TestPart2_1(t *testing.T) {
+	res := part2("1212")
+	exp := 6
+	if res != exp {
+		t.Errorf("result missmatch, got: %d, expect: %d.", res, exp)
+	}
+}
+
+func TestPart2_2(t *testing.T) {
+	res := part2("1221")
+	exp := 0
+	if res != exp {
+		t.Errorf("result missmatch, got: %d, expect: %d.", res, exp)
+	}
+}
+
+func TestPart2_3(t *testing.T) {
+	res := part2("123425")
+	exp := 4
+	if res != exp {
+		t.Errorf("result missmatch, got: %d, expect: %d.", res, exp)
+	}
+}
+
+func TestPart2_4(t *testing.T) {
+	res := part2("123123")
+	exp := 12
+	if res != exp {
+		t.Errorf("result missmatch, got: %d, expect: %d.", res, exp)
+	}
+}
+
+func TestPart2_5(t *testing.T) {
+	res := part2("12131415")
+	exp := 4
 	if res != exp {
 		t.Errorf("result missmatch, got: %d, expect: %d.", res, exp)
 	}
