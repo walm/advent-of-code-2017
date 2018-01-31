39dfee706b058083fc47573af7b03cd696aa0f97
Andreas Wålm <andreas@walm.net>
2018-01-31 06:51:15 +0100

    Day 10 - part 2

--- day10/main.go
+++ day10/main.go
@@ -54,8 +54,50 @@ func part1(size int, lengths []int) int {
 	return list[0] * list[1]
 }
 
+func part2(input string) string {
+	lengths := []int{}
+	for l := range input {
+		lengths = append(lengths, int(input[l]))
+	}
+	lengths = append(lengths, []int{17, 31, 73, 47, 23}...)
+
+	list := []int{}
+	for i := 0; i < 256; i++ {
+		list = append(list, i)
+	}
+
+	pos := 0
+	skip := 0
+	for round := 0; round < 64; round++ {
+		for _, l := range lengths {
+
+			reverse := make([]int, l)
+			for k := 0; k < l; k++ {
+				reverse[k] = list[(pos+k)%256]
+			}
+			for k := 0; k < l; k++ {
+				list[(pos+k)%256] = reverse[l-k-1]
+			}
+			pos = (pos + l + skip) % 256
+			skip++
+		}
+	}
+
+	var hash [16]byte
+	for c := 0; c < 16; c++ {
+		h := byte(list[c*16+0])
+		for v := 1; v < 16; v++ {
+			h = h ^ byte(list[c*16+v])
+		}
+		hash[c] = h
+	}
+
+	return fmt.Sprintf("%x", hash)
+}
+
 func main() {
 	input, _ := ioutil.ReadFile("input.txt")
 	data := satoia(strings.Split(string(input), ","))
 	fmt.Printf("Part1: %d\n", part1(256, data))
+	fmt.Printf("Part2: %s\n", part2(strings.Trim(string(input), "\n")))
 }
--- day10/main_test.go
+++ day10/main_test.go
@@ -9,3 +9,18 @@ func TestPart1_1(t *testing.T) {
 		t.Errorf("result missmatch, got: %d, expect: %d.", res, exp)
 	}
 }
+
+func TestPart2_1(t *testing.T) {
+	tests := map[string]string{
+		"":         "a2582a3a0e66e6e86e3812dcb672a272",
+		"AoC 2017": "33efeb34ea91902bb2f59c9920caa6cd",
+		"1,2,3":    "3efbe78a8d82f29979031a4aa0b16a9d",
+		"1,2,4":    "63960835bcdc130f0b66d7ff4f6a5a8e",
+	}
+	for inp, exp := range tests {
+		res := part2(inp)
+		if res != exp {
+			t.Errorf("result missmatch, got: %s, expect: %s.", res, exp)
+		}
+	}
+}
